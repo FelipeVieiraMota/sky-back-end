cd4efeca11c35addf05a6a5efb9431e8ba82d9aa
felipevieiramota <felipevieiramota@gmail.com>
2025-12-01 15:43:48 +0000

    Added postgres instead of H2

--- ms-authorization/src/main/resources/db/migration/V1__Default-User.sql
+++ ms-authorization/src/main/resources/db/migration/V1__Default-User.sql
@@ -1,11 +1,13 @@
 
 CREATE TABLE IF NOT EXISTS tb_users (
-  id       UUID PRIMARY KEY,
-  login    VARCHAR(255)  NOT NULL,
-  password VARCHAR(255)  NOT NULL,
-  role     VARCHAR(20)   NOT NULL
+  id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+  login VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  role VARCHAR(20) NOT NULL
 );
-CREATE UNIQUE INDEX IF NOT EXISTS ux_tb_users_login ON tb_users(login);
+
+CREATE UNIQUE INDEX IF NOT EXISTS ux_tb_users_login
+ON tb_users(login);
 
 --{
 --  "id": "d78258f3-ff6a-4720-8535-0dae096eb109",
@@ -15,4 +17,4 @@ CREATE UNIQUE INDEX IF NOT EXISTS ux_tb_users_login ON tb_users(login);
 --}
 -- password: 123
 INSERT INTO tb_users (id, login, password, role)
-VALUES (RANDOM_UUID(), 'felipe','$2a$10$KgBD.NMo29sVgcSLuzp54erMNO48L35aCF7uH2giJwEKUkzqbNjJO', 'ADMIN');
+VALUES (gen_random_uuid(), 'felipe', '$2a$10$KgBD.NMo29sVgcSLuzp54erMNO48L35aCF7uH2giJwEKUkzqbNjJO', 'ADMIN');
